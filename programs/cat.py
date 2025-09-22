da4e411216ce43da60dd2e3158ad635462331efa
Chaiwat Suttipongsakul <cwt@bashell.com>
2025-09-22 12:21:57 +0700

    cat accepts -n for line number

--- programs/cat.py
+++ programs/cat.py
@@ -1,3 +1,4 @@
+from kernel.utils import Parser
 from typing import Any, List
 from kernel.base_command import BaseFileCommand
 from kernel.common import resolve_path, handle_file_operation
@@ -8,15 +9,56 @@ class CatCommand(BaseFileCommand):
 
     def __init__(self) -> None:
         super().__init__("cat", "Concatenate files and print to stdout")
+        self.parser = Parser(
+            "cat",
+            name="Concatenate",
+            description="Concatenate the files at the given paths.",
+        )
+        pa = self.parser.add_argument
+        pa("paths", type=str, nargs="*")
+        pa("-n", "--number", action="store_true", dest="number", default=False)
 
-    def process_file(self, shell: Any, filepath: str) -> None:
+    def run(self, shell: Any, args: List[str]) -> None:
+        self.parser.add_shell(shell)
+        parsed_args = self.parser.parse_args(args)
+        if not self.parser.help:
+            if parsed_args.paths or (shell.stdin and self.supports_stdin):
+                # Process file arguments
+                for path in parsed_args.paths:
+                    self.process_file(shell, path, parsed_args.number)
+
+                # Process stdin if available
+                if shell.stdin and self.supports_stdin:
+                    self.process_stdin(shell, parsed_args.number)
+            else:
+                shell.stderr.write("missing file operand")
+
+    def process_file(
+        self, shell: Any, filepath: str, number_lines: bool = False
+    ) -> None:
         """Process a single file."""
         path = resolve_path(shell, filepath)
         f = handle_file_operation(shell, path, "open", "r")
         if f:
+            line_num = 1
             for line in f:
-                shell.stdout.write(line.rstrip())
+                if number_lines:
+                    shell.stdout.write(f"{line_num:6}  {line}")
+                    line_num += 1
+                else:
+                    shell.stdout.write(line.rstrip())
             f.close()
+
+    def process_stdin(self, shell: Any, number_lines: bool = False) -> None:
+        """Process input from stdin."""
+        if shell.stdin:
+            line_num = 1
+            for line in shell.stdin.read():
+                if number_lines:
+                    shell.stdout.write(f"{line_num:6}  {line}")
+                    line_num += 1
+                else:
+                    shell.stdout.write(line.rstrip())
 
     def help(self) -> str:
         return """
@@ -24,7 +66,10 @@ class CatCommand(BaseFileCommand):
 
     Concatenate the files at the given paths.
 
-    usage: cat [path]
+    usage: cat [options] [path]
+
+    Options:
+      -n, --number    Number all output lines
     """
 
 
